e52311de18259f3dd172cfaebea520fb179cfaf9
MofasserHossain <mofasserhossain1221@gmail.com>
2021-04-01 12:11:47 +0600

    file modify

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 
 // .MONGODB
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yedps.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -22,14 +23,58 @@ app.get('/', (req, res) => {
 
 client.connect((err) => {
   const foodsCollection = client.db('FoodBazar').collection('foods');
+  const ordersCollection = client.db('FoodBazar').collection('orders');
   console.log('connection Successfull');
   //.add foods
   app.post('/addProduct', (req, res) => {
     const food = req.body;
-    console.log(food);
+    // console.log(food);
     foodsCollection.insertOne(food).then((result) => {
       res.send(result.insertedCount > 0);
-      console.log(result);
+    });
+  });
+
+  // . read data
+  app.get('/allProducts', (req, res) => {
+    foodsCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  // . read specific data
+  app.get('/product/:id', (req, res) => {
+    foodsCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, item) => {
+        res.send(item);
+      });
+  });
+  // . delete product
+  app.delete('/delete/:id', (req, res) => {
+    console.log(req.params.id);
+    foodsCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        console.log(result);
+        res.send(result.deletedCount > 0);
+      });
+  });
+  // / order
+  // . add order
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    console.log(order);
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // . read order
+  app.get('/orders', (req, res) => {
+    const queryEmail = req.query.email;
+    console.log(queryEmail);
+    ordersCollection.find({ email: queryEmail }).toArray((err, documents) => {
+      res.send(documents);
     });
   });
 });
